4426fb5d303b8d338d38098d8a63a24dda284fd0
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-07 12:54:00 +0200

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,7 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.Factory;
+
+// https://www.tutorialspoint.com/design_pattern/factory_pattern.htm
+
+public interface Shape {
+    void draw();
+}
