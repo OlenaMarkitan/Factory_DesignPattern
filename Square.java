c63612bb0a696e48fc135c8621f3fff55cb46286
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-07 12:54:34 +0200

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,11 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.Factory;
+
+// https://www.tutorialspoint.com/design_pattern/factory_pattern.htm
+
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
